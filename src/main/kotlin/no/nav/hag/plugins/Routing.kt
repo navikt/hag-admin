b94bd717f4c9bc4d7115d19ec42f9b7c21cd3ff2
Morten Byhring <morten.byhring@nav.no>
2024-12-11 14:09:40 +0100

    brukernavn fix

--- src/main/kotlin/no/nav/hag/plugins/Routing.kt
+++ src/main/kotlin/no/nav/hag/plugins/Routing.kt
@@ -2,14 +2,20 @@ package no.nav.hag.plugins
 
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
-import io.ktor.server.application.*
+import io.ktor.server.application.Application
+import io.ktor.server.application.ApplicationCall
+import io.ktor.server.application.call
 import io.ktor.server.auth.authenticate
 import io.ktor.server.auth.authentication
 import io.ktor.server.html.respondHtml
 import io.ktor.server.http.content.staticResources
 import io.ktor.server.request.receiveParameters
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
+import io.ktor.server.response.respond
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.Routing
+import io.ktor.server.routing.get
+import io.ktor.server.routing.post
+import io.ktor.server.routing.routing
 import io.ktor.util.pipeline.PipelineContext
 import kotlinx.css.Color
 import kotlinx.css.CssBuilder
@@ -49,6 +55,7 @@ fun Application.configureRouting(notifikasjonService: NotifikasjonService) {
         }
         authenticate {
             get("/") {
+                val brukernavn = hentBrukernavnFraToken()
                 call.respondHtml(HttpStatusCode.OK) {
                     head {
                         link(rel = "stylesheet", href = "/styles.css", type = "text/css")
@@ -56,6 +63,11 @@ fun Application.configureRouting(notifikasjonService: NotifikasjonService) {
                     body {
                         h2 {
                             +"HAG Admin Tool"
+                        }
+                        p {
+                            text(
+                            "Logget inn som: $brukernavn"
+                            )
                         }
                         p {
                             a(href = "admin-ui/ferdigstillOppgave-form.html") {
@@ -78,8 +90,9 @@ fun Application.configureRouting(notifikasjonService: NotifikasjonService) {
                     return@post
                 }
                 try {
+                    val brukernavn = hentBrukernavnFraToken()
                     UUID.fromString(foresporselId)
-                    notifikasjonService.ferdigstillOppgave(foresporselId, auditUser())
+                    notifikasjonService.ferdigstillOppgave(foresporselId, brukernavn)
                 } catch (e: IllegalArgumentException) {
                     call.respond(HttpStatusCode.BadRequest)
                     return@post
@@ -105,8 +118,9 @@ fun Application.configureRouting(notifikasjonService: NotifikasjonService) {
                     return@post
                 }
                 try {
+                    val brukernavn = hentBrukernavnFraToken()
                     UUID.fromString(foresporselId)
-                    notifikasjonService.slettSak(foresporselId, auditUser())
+                    notifikasjonService.slettSak(foresporselId, brukernavn)
                 } catch (e: IllegalArgumentException) {
                     call.respond(HttpStatusCode.BadRequest)
                     return@post
@@ -128,9 +142,9 @@ fun Application.configureRouting(notifikasjonService: NotifikasjonService) {
     }
 }
 
-private fun PipelineContext<Unit, ApplicationCall>.auditUser(): String {
+private fun PipelineContext<Unit, ApplicationCall>.hentBrukernavnFraToken(): String {
     val principal: TokenValidationContextPrincipal? = call.authentication.principal()
-    return principal?.context?.getClaims("default")?.getStringClaim("NAVident") ?: "Unknown"
+    return principal?.context?.getClaims("employee")?.getStringClaim("NAVident") ?: "Unknown"
 }
 
 suspend inline fun ApplicationCall.respondCss(builder: CssBuilder.() -> Unit) {
--- src/test/kotlin/no/nav/hag/ApplicationTest.kt
+++ src/test/kotlin/no/nav/hag/ApplicationTest.kt
@@ -1,16 +1,19 @@
 package no.nav.hag
 
 import com.nimbusds.jwt.SignedJWT
-import io.ktor.client.request.*
-import io.ktor.http.*
-import io.ktor.server.testing.*
+import io.ktor.client.request.get
+import io.ktor.client.request.headers
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.testing.testApplication
 import io.ktor.test.dispatcher.testSuspend
-import kotlin.test.*
-import no.nav.hag.plugins.*
+import no.nav.hag.plugins.configureRouting
+import no.nav.hag.plugins.configureSecurity
 import no.nav.helsearbeidsgiver.arbeidsgivernotifikasjon.ArbeidsgiverNotifikasjonKlient
 import no.nav.security.mock.oauth2.MockOAuth2Server
 import no.nav.security.mock.oauth2.token.DefaultOAuth2TokenCallback
-import java.util.UUID
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ApplicationTest {
     @Test
@@ -23,20 +26,20 @@ class ApplicationTest {
             configureSecurity(disabled = false)
             configureRouting(FakeServiceImpl())
         }
-        val token: SignedJWT = server.issueToken(issuerId, Env.oauth2Environment.clientId, DefaultOAuth2TokenCallback(subject = "user123", audience = listOf(Env.oauth2Environment.clientId)))
+        val token: SignedJWT = server.issueToken(issuerId, Env.oauth2Environment.clientId, DefaultOAuth2TokenCallback(subject = "user123", audience = listOf(Env.oauth2Environment.clientId), claims = mapOf("NAVident" to "User123")))
         println("Token: ${token.serialize()}")
         client.get("/") {
             headers {
                 append("Authorization", "Bearer ${token.serialize()}")
             }
         }.apply {
+            println(bodyAsText())
             assertEquals(HttpStatusCode.OK, status)
         }
         client.get("/admin-ui/ferdigstillOppgave-form.html").apply {
             assertEquals(HttpStatusCode.OK, status)
+
         }
-        val uuid = UUID.randomUUID()
-        println(uuid)
         server.shutdown()
     }
 
