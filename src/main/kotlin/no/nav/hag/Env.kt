5049825488b57c68b8b0453a3405f443250dec70
mortenbyhring <117746154+mortenbyhring@users.noreply.github.com>
2025-10-09 10:02:23 +0200

    Update src/main/kotlin/no/nav/hag/Env.kt
    Co-authored-by: Mikael Bjerga <6940327+bjerga@users.noreply.github.com>

--- src/main/kotlin/no/nav/hag/Env.kt
+++ src/main/kotlin/no/nav/hag/Env.kt
@@ -5,7 +5,7 @@ object Env {
 
     val agNotifikasjonScope = System.getenv("ARBEIDSGIVER_NOTIFIKASJON_SCOPE") ?: "dummyscope"
     val agNotifikasjonUrl = System.getenv("ARBEIDSGIVER_NOTIFIKASJON_API_URL") ?: "http://dummy"
-    val utgaattUrl = System.getenv("SAK_UTGAATT_URL") ?: "http://expired" // link til statisk "Nav trenger ikke IM-side". Brukes ved "ferdigstill sak"
+    val utgaattUrl = System.getenv("SAK_UTGAATT_URL") ?: "http://expired" // link til statisk "Nav trenger ikke IM"-side. Brukes ved "ferdigstill sak"
 
     fun isTest(): Boolean {
         return isLocal() || clusterName == "dev-gcp"
