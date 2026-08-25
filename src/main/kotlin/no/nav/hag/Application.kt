db5bfc4b8e8eefc0b13ea290477e51a5b308dada
Morten Byhring <morten.byhring@nav.no>
2026-08-25 12:26:21 +0200

    fjernet mockService fra app

--- src/main/kotlin/no/nav/hag/Application.kt
+++ src/main/kotlin/no/nav/hag/Application.kt
@@ -14,6 +14,7 @@ import io.micrometer.core.instrument.binder.system.ProcessorMetrics
 import io.micrometer.prometheus.PrometheusConfig
 import io.micrometer.prometheus.PrometheusMeterRegistry
 import kotlinx.serialization.json.Json
+import no.nav.hag.kafkaproducer.KafkaConfig
 import no.nav.hag.plugins.configureRouting
 import no.nav.hag.plugins.configureSecurity
 import no.nav.helsearbeidsgiver.arbeidsgivernotifikasjon.ArbeidsgiverNotifikasjonKlient
@@ -64,7 +65,7 @@ fun Application.module() {
             sendevindu = Sendevindu.NKS_AAPNINGSTID,
         )
 
-    val forespoerselService = MockForespoerselService()
+    val forespoerselService = ForespoerselServiceImpl(KafkaConfig.getKafkaProducer())
 
     val notifikasjonService =
         when {
